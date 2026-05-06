e5cb2871de67a4d2538caeafe238d2849b24fbd5
Vi <vsg.sam@gmail.com>
2026-05-06 22:03:45 +0200

    added central crop saving

--- scripts/video_to_frames.py
+++ scripts/video_to_frames.py
@@ -25,6 +25,13 @@ def video_to_frames():
             break
         
         if count % FRAME_STEP == 0:
+            if USE_CENTRAL_CROP:
+                gap = 130
+                h, w = frame.shape[:2]
+                min_dim = min(h, w)
+                start_x = (w - min_dim) // 2 + gap
+                start_y = (h - min_dim) // 2 + gap
+                frame = frame[start_y:start_y+min_dim - gap*2, start_x:start_x+min_dim-gap*2]
             filename = os.path.join(OUTPUT_FOLDER, f"frame_{saved_count:04d}.jpg")
             cv2.imwrite(filename, frame)
             saved_count += 1
